844a8d9c863b58498e0f76f7667fe59c2358b386
muslimamaliha <muslimamaliha32@gmail.com>
2025-10-28 23:26:02 +0600

    Add files via upload

---
+++ [ICT20-A3]/ListExample.java
@@ -0,0 +1,18 @@
+import java.util.ArrayList;
+import java.util.Vector;
+
+public class ListExample {
+    public static void main(String[] args) {
+        // ArrayList Example
+        ArrayList<String> arrayList = new ArrayList<>();
+        arrayList.add("Apple");
+        arrayList.add("Banana");
+        System.out.println("ArrayList: " + arrayList);
+
+        // Vector Example
+        Vector<String> vector = new Vector<>();
+        vector.add("Mango");
+        vector.add("Orange");
+        System.out.println("Vector: " + vector);
+    }
+}
