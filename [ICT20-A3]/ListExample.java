b4a69527bba4141a94ceacbfb7ddb46b540af1ff
muslimamaliha <muslimamaliha32@gmail.com>
2025-10-28 23:41:19 +0600

    Update ListExample.java

--- [ICT20-A3]/ListExample.java
+++ [ICT20-A3]/ListExample.java
@@ -1,13 +1,20 @@
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Vector;
 
 public class ListExample {
     public static void main(String[] args) {
         // ArrayList Example
-        ArrayList<String> arrayList = new ArrayList<>();
+        ArrayList<String> arrayList = new ArrayList<>(Arrays.asList("Avocado", "Grapes", "Pineapple", "Date"));
         arrayList.add("Apple");
         arrayList.add("Banana");
+
+        System.out.println("Size: " + arrayList.size());
         System.out.println("ArrayList: " + arrayList);
+
+        arrayList.remove("Grapes");
+        System.out.println("ArrayList after removal: " + arrayList);
+        System.out.println("Size after removal: " + arrayList.size());
 
         // Vector Example
         Vector<String> vector = new Vector<>();
